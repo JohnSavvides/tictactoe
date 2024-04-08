ea89f436f7bf7f4d5bf42ffe03dbe548ff3bed77
Marshadillo <113390418+JohnSavvides@users.noreply.github.com>
2024-04-07 21:00:37 -0700

    Add files via upload

---
+++ TicTacToe/src/main/java/com/mycompany/tictactoe/TicTacToe.java
@@ -0,0 +1,17 @@
+package com.mycompany.tictactoe;
+
+/**
+ *
+ * @author John Savvides
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        TicTacToe ttt = new TicTacToe();
+}
+
+public TicTacToe() {
+    View view = new View();
+    view.setVisible(true);
+}
+}
